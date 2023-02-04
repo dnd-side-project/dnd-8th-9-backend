d3f7996bc85007f4b82f23434b62a15199095f87
YIHYUN HA <hihyun16@gmail.com>
2023-02-05 00:20:32 +0900

    chore: initial project setup

---
+++ src/main/java/com/team9ookie/dangdo/DangdoApplication.java
@@ -0,0 +1,13 @@
+package com.team9ookie.dangdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team9ookie/dangdo/DangdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team9ookie.dangdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
