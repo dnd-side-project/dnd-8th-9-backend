23fe258773077393901f35a00dd21276924c5229
YIHYUN HA <hihyun16@gmail.com>
2023-02-23 23:14:30 +0900

    refact: modify file upload path logic
    - 파일 업로드 시 file path를 FileType에서 조회
    - Issue #2

--- src/main/java/com/team9ookie/dangdo/service/FileService.java
+++ src/main/java/com/team9ookie/dangdo/service/FileService.java
@@ -1,9 +1,16 @@
 package com.team9ookie.dangdo.service;
 
 import com.team9ookie.dangdo.dto.file.FileDto;
+import com.team9ookie.dangdo.dto.file.FileType;
+import com.team9ookie.dangdo.entity.FileEntity;
 import com.team9ookie.dangdo.repository.FileRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -11,7 +18,23 @@ public class FileService {
 
     private final FileRepository fileRepository;
 
+    private final S3Service s3Service;
+
     public void saveFile(FileDto fileDto) {
         fileRepository.save(fileDto.toEntity());
     }
+
+    public List<FileEntity> createFileList(List<MultipartFile> fileList, FileType fileType, long targetId) throws IOException {
+        List<FileEntity> fileEntityList = new ArrayList<>();
+        for (MultipartFile file : fileList) {
+            String url = s3Service.upload(file, fileType.getFilePath());
+            fileEntityList.add(FileEntity.builder()
+                    .type(FileType.STORE_IMAGE)
+                    .url(url)
+                    .targetId(targetId)
+                    .build());
+        }
+        return fileEntityList;
+    }
+
 }
