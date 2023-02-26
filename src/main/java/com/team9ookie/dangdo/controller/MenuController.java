76e59932ca3b1297db8f9ccdd9e12248194ac236
minje0204 <minje@ajou.ac.kr>
2023-02-26 10:40:25 +0900

    feat: Menu API entity 복수형으로 ("s" 추가)

--- src/main/java/com/team9ookie/dangdo/controller/MenuController.java
+++ src/main/java/com/team9ookie/dangdo/controller/MenuController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/store/{storeId}/menu")
+@RequestMapping("/stores/{storeId}/menus")
 @AllArgsConstructor
 public class MenuController {
 
