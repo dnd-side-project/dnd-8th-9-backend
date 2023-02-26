106e409380205cd86d197d5ca544ba6025d9ba7a
minje0204 <minje@ajou.ac.kr>
2023-02-26 12:00:45 +0900

    feat: url store -> stores

--- src/main/java/com/team9ookie/dangdo/controller/StoreController.java
+++ src/main/java/com/team9ookie/dangdo/controller/StoreController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/store")
+@RequestMapping("/stores")
 @RequiredArgsConstructor
 public class StoreController {
 
