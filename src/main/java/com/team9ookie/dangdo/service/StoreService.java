94268cf1dbbf328bf196ec10de335eb25ac65ce2
YIHYUN HA <hihyun16@gmail.com>
2023-03-04 00:40:10 +0900

    refact: remove dummy data
    - Issue #25

--- src/main/java/com/team9ookie/dangdo/service/StoreService.java
+++ src/main/java/com/team9ookie/dangdo/service/StoreService.java
@@ -39,36 +39,6 @@ public class StoreService {
 
     private final FileRepository fileRepository;
 
-    // TODO: 테스트용 예제 데이터 생성 코드. 더미 데이터 생성 기능 구현 후 제거
-    @PostConstruct
-    public void initData() {
-        if (storeRepository.count() > 0) return;
-
-        List<Store> exampleStoreList = IntStream.range(1, 4).boxed().map(index -> Store.builder()
-                .name("당도케이크 [%s]".formatted(index))
-                .location("서울특별시 당도구 당도로 5길 9")
-                .businessHours("11시-20시")
-                .orderForm("성함/연락처: \n픽업 날짜, 시간: 월 일 요일 시 분 \n크기: 호 \n맛: \n디자인: 케이크 사진을 보내주세요! \n하트 or 원형 : \n케이크위 레터링: \n케이크밑판문구: \n요청사항:")
-                .notice("- 사용되는 꼿은 식용꽃으로 랜덤으로 사용됩니다. 꽃잎 컬러 및 모양은 지정 불가합니다. \n*요청사항에 기재하셔도 변경 내용은 적용되지 않습니다. \n- 사용된 조화는 수급에 따라 비슷한 조화로 대체될수 있습니다. \n- 먼슬리 디자인은 디자인 및 케이크 칼라, 레터링 칼라, 레터링 위치 등 변경 불가합니다. \n*요청사항에 기재하셔도 변경 내용은 적용되지 않습니다. \n- 6,7,8 번 디자인은 사이즈업 불가합니다. \n- 1,3,6,7,8,번은 영문 레터링만 가능합니다.(한글불가) *한글로 기재시 기본 문구로 제작 됩니다. \n- 모든 식용 꽃은 세척 및 소독후 사용됩니다. 꽃잎은 식용이라 드실수 있지만 질감의 이질감을 줄수 있어서 떼고 드시는걸 추천드립니다. \n- 진주는 슈가로 만들어진 스프링클입니다. 크기가 큰 스프링클은 씹어 드실경우 치아 손상이 있을 수 있으니 제거하고 드세요.")
-                .canPickup(true)
-                .canDelivery(false)
-                .category("레터링 케이크,캐릭터 케이크")
-                .build()).toList();
-        storeRepository.saveAll(exampleStoreList);
-
-        List<StoreLink> exampleStoreLinkList = Arrays.asList(
-                StoreLink.builder().platform(Platform.INSTAGRAM).url("https://example-store.com").store(exampleStoreList.get(0)).build(),
-                StoreLink.builder().platform(Platform.KAKAO).url("https://example-store.com").store(exampleStoreList.get(1)).build()
-        );
-        storeLinkRepository.saveAll(exampleStoreLinkList);
-
-        List<FileEntity> exampleFileEntityList = Arrays.asList(
-                FileEntity.builder().type(FileType.STORE_IMAGE).url("https://example-file.com").targetId(1L).build(),
-                FileEntity.builder().type(FileType.STORE_IMAGE).url("https://example-file.com").targetId(3L).build()
-        );
-        fileRepository.saveAll(exampleFileEntityList);
-    }
-
     @Transactional(readOnly = true)
     public List<StoreResponseDto> getAll(StoreConditionDto conditionDto) {
         List<StoreDetailDto> storeDetailDtoList = customStoreRepository.getStoreListByCondition(conditionDto);
