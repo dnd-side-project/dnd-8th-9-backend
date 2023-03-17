c7734483a7f520c0320c1330cf0394897b1e32bb
minje0204 <minje@ajou.ac.kr>
2023-03-17 20:58:57 +0900

    feat: make User email unique

--- src/main/java/com/team9ookie/dangdo/entity/User.java
+++ src/main/java/com/team9ookie/dangdo/entity/User.java
@@ -18,7 +18,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(unique=true)
     private String email;
 
     @Column
