44d7b84ae98f2e7e336854eb4f0bacf49be0ca00
YIHYUN HA <hihyun16@gmail.com>
2023-05-06 22:03:36 +0900

    feat: apply modified principal creation logic
    - 변경된 principal 획득 로직을 적용
    - Issue #54

--- src/main/java/com/team9ookie/dangdo/controller/UserController.java
+++ src/main/java/com/team9ookie/dangdo/controller/UserController.java
@@ -1,20 +1,24 @@
 package com.team9ookie.dangdo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team9ookie.dangdo.auth.common.ApiResponse;
 import com.team9ookie.dangdo.config.AppProperties;
 import com.team9ookie.dangdo.dto.BaseResponseDto;
+import com.team9ookie.dangdo.dto.menu.MenuConditionDto;
 import com.team9ookie.dangdo.dto.menu.MenuResponseListDto;
+import com.team9ookie.dangdo.dto.store.StoreConditionDto;
 import com.team9ookie.dangdo.dto.store.StoreListResponseDto;
 import com.team9ookie.dangdo.entity.User;
-import com.team9ookie.dangdo.service.MenuBookmarkService;
-import com.team9ookie.dangdo.service.StoreBookmarkService;
+import com.team9ookie.dangdo.service.MenuService;
+import com.team9ookie.dangdo.service.StoreService;
 import com.team9ookie.dangdo.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,10 +29,12 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-
     private final AppProperties appProperties;
-    private final StoreBookmarkService storeBookmarkService;
-    private final MenuBookmarkService menuBookmarkService;
+
+    private final StoreService storeService;
+    private final MenuService menuService;
+
+    private final ObjectMapper objectMapper;
 
     @GetMapping
     public ApiResponse getUser() {
@@ -44,24 +50,42 @@ public class UserController {
     }
 
     @GetMapping("/store-bookmarks")
-    public ResponseEntity<BaseResponseDto<List<StoreListResponseDto>>> findStoreBookmarkList() {
-        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-
-        User user = userService.getUser(principal.getUsername());
-
-        return ResponseEntity.ok(BaseResponseDto.ok(storeBookmarkService.findMarkedStoreList(user)));
+    public ResponseEntity<BaseResponseDto<List<StoreListResponseDto>>> findStoreBookmarkList(@RequestParam(name = "cond", required = false) String condition) {
+        org.springframework.security.core.userdetails.User principal =
+                (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try {
+            StoreConditionDto conditionDto;
+            if (condition == null) {
+                conditionDto = new StoreConditionDto();
+            } else {
+                conditionDto = objectMapper.readValue(condition, StoreConditionDto.class);
+            }
+            User user = userService.getUser(principal.getUsername());
+            conditionDto.setUserId(user.getId());
+            return ResponseEntity.ok(BaseResponseDto.ok(storeService.findAll(conditionDto)));
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("유효하지 않은 필터링 형식 cond=" + condition);
+        }
     }
 
     @GetMapping("/menu-bookmarks")
-    public ResponseEntity<BaseResponseDto<List<MenuResponseListDto>>> findMenuBookmarkList() {
-        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public ResponseEntity<BaseResponseDto<List<MenuResponseListDto>>> findMenuBookmarkList(@RequestParam(name = "cond", required = false) String condition) {
+        org.springframework.security.core.userdetails.User principal =
+                (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        if (principal == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-
-        User user = userService.getUser(principal.getUsername());
-
-        return ResponseEntity.ok(BaseResponseDto.ok(menuBookmarkService.findMarkedMenuList(user)));
+        try {
+            MenuConditionDto conditionDto;
+            if (condition == null) {
+                conditionDto = new MenuConditionDto();
+            } else {
+                conditionDto = objectMapper.readValue(condition, MenuConditionDto.class);
+            }
+            User user = userService.getUser(principal.getUsername());
+            conditionDto.setUserId(user.getId());
+            return ResponseEntity.ok(BaseResponseDto.ok(menuService.findAll(conditionDto)));
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("유효하지 않은 필터링 형식 cond=" + condition);
+        }
     }
 
 }
